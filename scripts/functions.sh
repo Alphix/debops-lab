f711fabf12d1dd244992a38bcb145ca8f9f5dbbc
David Härdeman <david@hardeman.nu>
2023-09-24 15:02:41 +0200

    Some minor cleanups, icinga is serialised on the playbook level now

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -83,18 +83,6 @@ function die {
 }
 
 
-# FIXME: Remove
-function info {
-	printf '%s\n' "$*"
-}
-
-
-# FIXME: Remove
-function error {
-	printf '%s\n' "$*" >&2
-}
-
-
 function activate_python_virtualenv {
 	#sudo apt install build-essential python3-virtualenv virtualenv python3-dev \
         #         libffi-dev libssl-dev libsasl2-dev libldap2-dev python3-pip
